1d7cd35fc2482682c8a26ac493c6b5d03e7296eb
Igor Melo <igoracmelo@protonmail.com>
2023-12-28 15:28:51 -0300

    red: server must be reached

--- ttcsv/ttcsv_test.go
+++ ttcsv/ttcsv_test.go
@@ -1,6 +1,7 @@
 package ttcsv
 
 import (
+	th "anyflix/testhelper"
 	"anyflix/ttsearch"
 	"net/http"
 	"testing"
@@ -10,7 +11,14 @@ func TestSearch(t *testing.T) {
 	httpClient := &http.Client{}
 	cl := NewClient(httpClient)
 
+	reached := false
+	httpClient.Transport = th.RoundTripFunc(func(req *http.Request) *http.Response {
+		reached = true
+		return &http.Response{}
+	})
+
 	_, _ = cl.Search(ttsearch.SearchParams{
 		Query: "south park",
 	})
+	th.Assert(t, reached, "server not reached")
 }
