6c526d88d75bcbeb1b7e5337e006c9c961c14eac
Igor Melo <igoracmelo@protonmail.com>
2023-12-28 16:17:38 -0300

    fix: ensure query is correct

--- ttcsv/ttcsv.go
+++ ttcsv/ttcsv.go
@@ -2,7 +2,9 @@ package ttcsv
 
 import (
 	"anyflix/ttsearch"
+	"fmt"
 	"net/http"
+	"net/url"
 )
 
 var _ ttsearch.Searcher = Client{}
@@ -21,6 +23,12 @@ func NewClient(httpClient *http.Client) Client {
 
 // Search implements ttsearch.Searcher.
 func (cl Client) Search(params ttsearch.SearchParams) (res []ttsearch.Result, err error) {
-	_, err = cl.HTTP.Get(cl.BaseURL + "/search")
+	q := url.Values{}
+	q.Set("q", params.Query)
+	q.Set("page", fmt.Sprint(params.Page))
+	q.Set("size", fmt.Sprint(params.Size))
+
+	req, _ := http.NewRequest("GET", cl.BaseURL+"/search?"+q.Encode(), nil)
+	_, err = cl.HTTP.Do(req)
 	return
 }
--- ttcsv/ttcsv_test.go
+++ ttcsv/ttcsv_test.go
@@ -3,6 +3,7 @@ package ttcsv
 import (
 	th "anyflix/testhelper"
 	"anyflix/ttsearch"
+	"fmt"
 	"net/http"
 	"testing"
 )
@@ -11,17 +12,27 @@ func TestSearch(t *testing.T) {
 	httpClient := &http.Client{}
 	cl := NewClient(httpClient)
 
+	params := ttsearch.SearchParams{
+		Query: "south park",
+		Page:  5,
+		Size:  20,
+	}
+
 	reached := false
 	httpClient.Transport = th.RoundTripFunc(func(req *http.Request) *http.Response {
 		reached = true
 		th.AssertEqual(t, "torrents-csv.com", req.URL.Host)
 		th.AssertEqual(t, "/service/search", req.URL.Path)
+
+		q := req.URL.Query()
+		th.AssertEqual(t, params.Query, q.Get("q"))
+		th.AssertEqual(t, fmt.Sprint(params.Page), q.Get("page"))
+		th.AssertEqual(t, fmt.Sprint(params.Size), q.Get("size"))
+
 		return &http.Response{}
 	})
 
-	_, err := cl.Search(ttsearch.SearchParams{
-		Query: "south park",
-	})
+	_, err := cl.Search(params)
 	th.Assert(t, reached, "server not reached")
 	th.AssertEqual(t, nil, err)
 }
