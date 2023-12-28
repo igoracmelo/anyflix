10ea2ae84a63ed4c81b66900b921f5dc4bcaba61
Igor Melo <igoracmelo@protonmail.com>
2023-12-28 15:30:23 -0300

    green: server must be reached

--- ttcsv/ttcsv.go
+++ ttcsv/ttcsv.go
@@ -21,5 +21,6 @@ func NewClient(httpClient *http.Client) Client {
 
 // Search implements ttsearch.Searcher.
 func (cl Client) Search(params ttsearch.SearchParams) (res []ttsearch.Result, err error) {
+	_, err = cl.HTTP.Get("/")
 	return
 }
